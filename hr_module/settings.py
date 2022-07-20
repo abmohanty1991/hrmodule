10f7b1de99fec2c4b000844972df26b884b36e45
abhi <abmohanty1991@gmail.com>
2022-07-20 15:52:37 +0530

    Update settings.py

--- hr_module/settings.py
+++ hr_module/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'hr_module.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.oracle',
-#         'NAME': 'xe',
-#         'USER': 'system',
-#         'PASSWORD':'hr',
-#         'HOST':'localhost',
-#         'PORT':'1521'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# } 
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe',
+        'USER': 'system',
+        'PASSWORD':'hr',
+        'HOST':'localhost',
+        'PORT':'1521'
+    }
+} 
 #
 # DATABASES = {
 #     'default': {
